a3c92f7bcd4b7090df0bd59dab1f6d951363b012
Nikolozi <elbakidzen78@gmail.com>
2025-09-15 23:21:56 +0400

    Add my project files

---
+++ Homework 4/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Homework_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine("Nikoloz Elbakidze");
+            
+            Console.WriteLine (Console.ReadLine());        }
+    }
+}
